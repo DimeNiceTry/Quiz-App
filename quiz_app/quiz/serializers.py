281e13e63bed7686a542b3f6b73c55f3d89bef45
DimeNiceTry <chdd04@gmail.com>
2025-03-17 01:06:00 +0300

    Добавлен просмотр результатов тестов. Обновлен интерфейс

--- quiz_app/quiz/serializers.py
+++ quiz_app/quiz/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Quiz, Question, Answer
+from .models import Quiz, Question, Answer, QuizResult
+from django.contrib.auth.models import User
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,15 +15,30 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ('id', 'text', 'answers')
 
 class QuizSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    questions_count = serializers.SerializerMethodField()
+    
     class Meta:
         model = Quiz
-        fields = ('id', 'title', 'author', 'created_at')
-        read_only_fields = ('author',) # Автор устанавливается автоматически, а не из запроса
+        fields = ('id', 'title', 'author', 'created_at', 'questions_count')
+    
+    def get_questions_count(self, obj):
+        return obj.questions.count()
 
 class QuizDetailSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True)
+    author = serializers.ReadOnlyField(source='author.username')
     
     class Meta:
         model = Quiz
         fields = ('id', 'title', 'author', 'created_at', 'questions')
         read_only_fields = ('author',)
+
+class QuizResultSerializer(serializers.ModelSerializer):
+    quiz_title = serializers.ReadOnlyField(source='quiz.title')
+    username = serializers.ReadOnlyField(source='user.username')
+    
+    class Meta:
+        model = QuizResult
+        fields = ['id', 'quiz', 'quiz_title', 'user', 'username', 'score', 'max_score', 'completed_at']
+        read_only_fields = ['completed_at']
